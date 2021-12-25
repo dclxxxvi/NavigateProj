0289452290b1cecba72dd5db0902f07e9c7e3528
UralGuru <gizzatullinu@gmail.com>
2021-12-25 18:00:22 +0500

    style

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -7,7 +7,12 @@ function Header() {
             <div className="container">
                 <nav className="nav">
                     <a href="https://urfu.ru" className="urfu_logo" target="_blank"></a>
-                    <a className="phone_link" href="tel: +7 (343) 375-44-44">+7 (343) 375-44-44</a>
+                    <div className="phone_links">
+                        <a href="/" className="phone_link lk"></a>
+                        <a className="phone_link" href="tel: +7 (343) 375-44-44">+7 (343) 375-44-44</a>
+                        <a className="phone_link" href="tel: +7 (343) 375-44-44">+7 (343) 375-44-44</a>
+                    </div>
+                    
                 </nav>
             </div>
         </header>
